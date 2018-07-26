9decdeb00df5f614a34f715fefa852c84f2af6d0
idzia <ania.idzi@gmail.com>
2018-07-26 17:01:29 +0200

    Add student store and transaction_history

--- src/main/java/com/codecool/queststore/App.java
+++ src/main/java/com/codecool/queststore/App.java
@@ -13,6 +13,9 @@ public class App {
 
         server.createContext("/login", new Login());
         server.createContext("/student", new StudentProfile());
+        server.createContext("/store", new StudentStore());
+        server.createContext("/transaction_history", new StudentHistory());
+        server.createContext("/incubator", new StudentIncubator());
         server.createContext("/mentor", new MentorProfile());
         server.createContext("/admin", new AdminProfile());
         server.createContext("/static", new Static());
